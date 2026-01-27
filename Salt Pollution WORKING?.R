0f2251ef8a04d809ef4992c3f537f129bd2142fc
avilleda26 <avilleda@luc.edu>
2026-01-27 11:09:57 -0600

    Basic Analysis

--- Salt Pollution WORKING?.R
+++ Salt Pollution WORKING?.R
@@ -9,7 +9,20 @@ file_path <- "Consulting_ClassUMBS_Mesocosm_Biochar_NaCl_Master_Data_2025aug27.x
 df <- read_excel(file_path)
 
 
+df <- df %>%
+  mutate(
+    tankSize = as.factor(tankSize),
+    biocharRate_trt = as.factor(biocharRate_trt),
+    saltRate_trt = as.factor(saltRate_trt),
+    treatment = as.factor(treatment),
+    year = as.integer(year),
+    month = as.integer(month),
+    jdate = as.integer(jdate)
+  )
 
+# Quick check
+str(df)
+head(df)
 
 
  
