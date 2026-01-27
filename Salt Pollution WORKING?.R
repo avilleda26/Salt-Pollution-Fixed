6e882f40e92597b1d52a583bba4c5f6756b0ae73
avilleda26 <avilleda@luc.edu>
2026-01-27 10:27:21 -0600

    import

--- Salt Pollution WORKING?.R
+++ Salt Pollution WORKING?.R
@@ -1,3 +1,15 @@
-#hello hello
+
+
+library(readxl)
+library(dplyr)
+
+
+file_path <- "Consulting_ClassUMBS_Mesocosm_Biochar_NaCl_Master_Data_2025aug27.xlsx"
+
+df <- read_excel(file_path)
+
+
+
+
 
  
